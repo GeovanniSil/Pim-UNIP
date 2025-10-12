a39589ea44dc7d34b18a08f7a95413b3a2195577
GeovanniSil <geovannisilvadias@gmail.com>
2025-10-12 09:56:25 -0300

    filtro

--- Pim - verdadeiro/filtro_aluno.c
+++ Pim - verdadeiro/filtro_aluno.c
@@ -10,37 +10,47 @@ int main(){
         printf("erro");
     }
     
+    //char aluno[15];
+    //printf("digite o aluno:\n");
+    //scanf("%s", aluno);
+    
     char frase[70];
-    char aluno[15];
-    printf("digite o aluno:\n");
-    scanf("%s", aluno);
+    char * ponteiro;
+    //int encontrou = 0;
 
-    //char alunotxt1[15];
-    //char alunotxt2[15];
-    //char alunotxt3[15];
-    //char alunotxt4[15];
-    int encontrou = 0;
-    //char alunotxt2[15];
+    //função strtok, strtok serve para separar uma string em partes menores usando um caractere delimitador. No seu código, ele está separando cada campo da linha do arquivo para comparar o nome do aluno
     while (fgets(frase, sizeof(frase), pim01) != NULL)
     {
+        ponteiro = strtok(frase, ";\n");
         frase[strcspn(frase, "\n")] = '\0';
-        char *nome = strtok(frase, ";");
-        //função strtok, strtok serve para separar uma string em partes menores usando um caractere delimitador. No seu código, ele está separando cada campo da linha do arquivo para comparar o nome do aluno
-        while (nome != NULL) {
-            if (strcmp(nome, aluno) == 0) {
-                printf("Aluno encontrado: %s\n", nome);
-                encontrou = 1;
-                break;
-            }/* else
-                printf("nao encontrado");    
-                encontrou = 1;
-                break; */
-            nome = strtok(NULL, ";");
-            if(!encontrou);
-                printf("nao");
-                break;
-    }
-    if (encontrou) break; // sai do loop principal
+        while (ponteiro)
+        {
+            printf("%s\n", ponteiro);
+            ponteiro = strtok(NULL, ";\n");
+            
+        }
 }
 fclose(pim01);
+} 
+
+/* #include <stdio.h>
+#include <string.h>
+
+int main(){
+//aprendendo sobre a função strtok
+
+    char palavra[50] = "Bom dia cinderela!";
+    char *ponteiro;
+
+    ponteiro = strtok(palavra, " "); //recebe 2 parametro, primeiro parametro é qual é a string que eu quero dividir e o segundo é de que forma queremos dividir essa string 
+
+    while (ponteiro)//usamos o loop para fazer com que ele divida a string toda e nao somente a primeira palavra, sem usar o loop, ela vai dividr apenas a primeira palavra
+    {
+        printf("token %s\n", ponteiro);
+        ponteiro = strtok(NULL, " ");
+    }
+    
+
+    return 0;
 }
+ */
