199c73a4d758d754031c20211199ccf2ebc3b08d
GeovanniSil <geovannisilvadias@gmail.com>
2025-11-13 13:50:25 -0300

    .

--- Pim - verdadeiro/infernoErro.c
+++ Pim - verdadeiro/infernoErro.c
@@ -6,6 +6,10 @@ int nomeValido(char *nome);
 int buscarNosArquivos(FILE *pim01, FILE *pim02, FILE *pim03, FILE *pim04, char *nomeAluno);
 void cores(int nota1);
 
+FILE *pim01 = fope("C:\\Users\\adilsondias\\OneDrive\\Desktop\\Pim\\Pim-UNIP\\Algoritmos_e_Estruturas_de_Dados_em_Pythocsv""r");
+FILE *pim02 = fopen("C:\\Users\\adilsondias\\OneDrive\\Desktop\\Pim\\Pim-UNIP\\Analise_e_Projeto_de_Sistemas.csv", "r");
+FILE *pim03 = fopen("C:\\Users\\adilsondias\\OneDrive\\Desktop\\Pim\\Pim-UNIP\\Engenharia_de_Software_Agil.csv", "r");
+FILE *pim04 = fopen("C:\\Users\\adilsondias\\OneDrive\\Desktop\\Pim\\Pim-UNIP\\Programacao_Estruturada_em_C.csv", "r");
 
 int main()
 
